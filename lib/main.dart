a31c3e963a0c82c809aa08d7b09baea35b1c147a
kirankamal20 <105903615+kirankamal20@users.noreply.github.com>
2022-08-18 21:41:33 -0700

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:calzada/app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+// ffffff
 import 'app/routes/app_pages.dart';
 
 void main() {
